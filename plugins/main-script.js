9eff273bfcb21814ef6316ead494c9f9242d27da
Moonlitwhisperske <167529772+Moonlitwhisperske@users.noreply.github.com>
2024-05-12 10:34:39 +0300

    Update main-script.js

--- plugins/main-script.js
+++ plugins/main-script.js
@@ -3,7 +3,7 @@ import { join } from 'path';
 import axios from 'axios'; 
 
 let handler = async function (m, { conn, __dirname }) {
-  const githubRepoURL = 'https://github.com/PRINCE-GDS/THE-PRINCE-BOT';
+  const githubRepoURL = 'https://github.com/Moonlitwhisperske/GC-MD-BOT';
 
   try {
   
@@ -16,7 +16,7 @@ let handler = async function (m, { conn, __dirname }) {
 
       // Format the repository information with emojis
       const formattedInfo = `
-╭⊱✫🛡️ᑭᖇIᑎᑕᕮ ᗷOT🛡️✫⊱╮
+╭⊱✫🛡️GC-MD-BOT BY GABRIEL KORIR 🛡️✫⊱╮
 │✫ - *📂Repository Name: ${repoData.name}*
 │✫ - *📃Description: ${repoData.description}*
 │✫ - *🛡️Owner: ${repoData.owner.login}*
